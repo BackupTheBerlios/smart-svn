1d3a0fbfdf6fd398d3f57c3e620ecb94d9cac691
atu <atu@5b7d10fb-7edc-0310-8ffd-a5e447ab9178>
2004-06-16 15:01:59 +0000

    moved load options to the option module

--- admin/include/base.inc.php
+++ admin/include/base.inc.php
@@ -35,8 +35,7 @@ if( SF_OB == TRUE )
 // The base object
 include_once( SF_BASE_DIR . '/admin/include/class.sfObject.php' );
 $base = & new sfObject;
-if( SF_DEBUG == TRUE )
-    $base->register( 'base', __FILE__, __LINE__);
+if( SF_DEBUG == TRUE ) $base->register( 'base', __FILE__, __LINE__);//remove
 
 // include pear db
 include_once( 'DB.php' );
@@ -52,20 +51,17 @@ include_once( SF_BASE_DIR . '/admin/lib/patError/patErrorManager.php' );
 
 // set error handler
 $base->errorHandler   =  new sfErrorHandler();
-if( SF_DEBUG == TRUE )
-    $base->register( 'errorHandler', __FILE__, __LINE__);
+if( SF_DEBUG == TRUE ) $base->register( 'errorHandler', __FILE__, __LINE__);//remove
+    
 patErrorManager::setErrorHandling( E_ALL , 'callback', array( $base->errorHandler, 'sfDebug' ) );
 
-// The util object
+// Load the util class
 include_once( SF_BASE_DIR . '/admin/include/class.sfUtil.php' );
-$base->util = & new sfUtil;
-if( SF_DEBUG == TRUE )
-    $base->register( 'util', __FILE__, __LINE__);
 
 /*
  * The base location to Smart
  */    
-define('SF_BASE_LOCATION', $base->util->base_location());
+define('SF_BASE_LOCATION', sfUtil::base_location());
 
 // Check if setup was done
 if(!@is_file(SF_BASE_DIR . '/admin/config/config_db_connect.xml.php'))
@@ -86,57 +82,10 @@ if(!@is_file(SF_BASE_DIR . '/admin/config/config_db_connect.xml.php'))
 include_once( SF_BASE_DIR . '/admin/include/class.sfEvent.php' );
 // include patTemplate
 include_once( SF_BASE_DIR . '/admin/lib/patTemplate/patTemplate.php' );
-// include patConfiguration
-include_once( SF_BASE_DIR . '/admin/lib/patConfiguration/include/patConfiguration.php' );
-
-// pat configurator instance    
-$base->conf = new patConfiguration;
-if( SF_DEBUG == TRUE )
-    $base->register( 'conf', __FILE__, __LINE__);
-
-//  set global config dir
-$base->conf->setConfigDir( SF_BASE_DIR . '/admin/config' );
-
-//
-// Load base options
-//
-
-//  parse global config options file
-$base->conf->parseConfigFile( 'config_options.xml' );
-
-//  get the db name set
-$base->db_connect = $base->conf->getConfigValue('option.db');
-
-//  get the public template folder name
-$base->tpl_folder = $base->conf->getConfigValue('option.templates_folder');
-
-// get charset
-$base->charset = $base->conf->getConfigValue('option.charset');
-
-// get css folder
-$base->design_style = $base->conf->getConfigValue('option.design_style');
-
-//  parse config db file
-$base->conf->parseConfigFile( 'config_db_connect.xml.php' );
-    
-//  get all config db values
-$base->db_data = $base->conf->getConfigValue( 'db.' . $base->db_connect );
-
-// set db resource
-$base->dsn = $base->db_data['db_type'].'://'.$base->db_data['db_user'].':'.$base->db_data['db_passwd'].'@'.$base->db_data['db_host'].'/'.$base->db_data['db_name'].'';
-
-// db instance and connect
-$base->db = & DB::connect($base->dsn);
-if (DB::isError($base->db)) 
-{
-    patErrorManager::raiseError( "db:connect", $base->db->getMessage(), "File: ".__FILE__."\nLine: ".__LINE__ );
-}
-unset($base->dsn);
 
 //  instance of patTemplate
 $base->tpl = new patTemplate();
-if( SF_DEBUG == TRUE )
-    $base->register( 'tpl', __FILE__, __LINE__);
+if( SF_DEBUG == TRUE ) $base->register( 'tpl', __FILE__, __LINE__);//remove
 
 // set templates root dir
 $base->tpl->setRoot( SF_BASE_DIR  );
@@ -147,9 +96,10 @@ $base->tpl->useTemplateCache( 'File', array('cacheFolder' => SF_BASE_DIR . '/adm
 
 // Event class instance
 $base->event = & new sfEvent;
-if( SF_DEBUG == TRUE )
-    $base->register( 'event', __FILE__, __LINE__);
-    
+if( SF_DEBUG == TRUE ) $base->register( 'event', __FILE__, __LINE__);//remove
+
+// Register all handlers
+//
 // register system event handler
 include_once (SF_BASE_DIR . '/admin/include/event_handler_' . SF_SECTION . '.php');
 
@@ -175,5 +125,21 @@ $base->tmp_directory->close();
 unset($base->tmp_evt_handler);
 unset($base->tmp_directory);
 unset($base->tmp_evt_handler);
+
+//
+// Load base options
+//
+$base->event->directed_run( SF_OPTION_MODULE, SF_EVT_LOAD_INIT_OPTION );
+
+// set db resource
+$base->dsn = $base->db_data['db_type'].'://'.$base->db_data['db_user'].':'.$base->db_data['db_passwd'].'@'.$base->db_data['db_host'].'/'.$base->db_data['db_name'].'';
+
+// db instance and connect
+$base->db = & DB::connect($base->dsn);
+if (DB::isError($base->db)) 
+{
+    patErrorManager::raiseError( "db:connect", $base->db->getMessage(), "File: ".__FILE__."\nLine: ".__LINE__ );
+}
+unset($base->dsn);
 
 ?>
