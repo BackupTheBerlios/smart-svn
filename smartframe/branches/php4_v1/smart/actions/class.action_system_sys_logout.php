32565d7d69790be75299da8dd039182594f79436
atu <atu@5b7d10fb-7edc-0310-8ffd-a5e447ab9178>
2005-04-18 12:07:58 +0000

    PHP 4 Version of SMART

---
+++ smartframe/branches/php4_v1/smart/actions/class.action_system_sys_logout.php
@@ -0,0 +1,32 @@
+<?php
+// ----------------------------------------------------------------------
+// Smart PHP Framework
+// Copyright (c) 2004, 2005
+// by Armand Turpel < smart@open-publisher.net >
+// http://smart.open-publisher.net/
+// ----------------------------------------------------------------------
+// GNU LESSER GENERAL PUBLIC LICENSE
+// To read the license please visit http://www.gnu.org/licenses/lgpl.txt
+// ----------------------------------------------------------------------
+
+/**
+ * action_system_sys_logout class 
+ *
+ */
+ 
+class action_system_sys_logout extends action
+{
+    /**
+     * If a logout request was done
+     *
+     * @param array $data
+     */
+    function perform( $data )
+    {            
+        // include here additional clean up code. 
+
+        return SF_IS_VALID_ACTION;
+    }    
+}
+
+?>
