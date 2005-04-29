78308f0369c1041745f332ad4f6f643906325da5
atu <atu@5b7d10fb-7edc-0310-8ffd-a5e447ab9178>
2005-04-29 11:17:28 +0000

    Added web end user error view and template

---
+++ smartframe/trunk/views_default/ViewError.php
@@ -0,0 +1,36 @@
+<?php
+// ----------------------------------------------------------------------
+// Smart PHP Framework
+// Copyright (c) 2004
+// by Armand Turpel < smart@open-publisher.net >
+// http://smart.open-publisher.net/
+// ----------------------------------------------------------------------
+// LICENSE GPL
+// To read the license please visit http://www.gnu.org/copyleft/gpl.html
+// ----------------------------------------------------------------------
+
+/**
+ * ViewError class
+ */
+
+class ViewError extends SmartView
+{
+     /**
+     * Template of this view
+     * @var string $template
+     */
+    public $template = 'error';
+
+    /**
+     * Fetch error string
+     *
+     */
+    function perform()
+    {
+        // assign error as template variable
+        // 
+        $this->tplVar = $this->viewData;
+    }
+}
+
+?>
